93997d907f3add20e6a75b60f1e87db72572a727
Jonathan Feudi <jonathanfeudi@gmail.com>
2016-02-02 16:28:20 -0500

    makeSideDecks working, creating side deck cards and pushing to sideDeck arrays. Two arrays inside sideDecks, one for each player

--- js/script.js
+++ js/script.js
@@ -7,6 +7,10 @@ function Card(){
   //side or main
 };
 
+function randomInt(min, max){
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 function makeMainDeck(){
   for (var j = 0; j < 4; j++){
     for(var i = 1; i < 11; i++){
@@ -19,6 +23,24 @@ function makeMainDeck(){
   }
 };
 
+function makeSideDeck(){
+  for (var j = 0; j < 2; j++){
+    console.log(j);
+    for (var i = 0; i < 10; i++){
+      var cardValueRandom = randomInt(1, 7);
+      var newCard = new Card();
+      newCard.val = cardValueRandom;
+      newCard.operation = '+';
+      newCard.deck = 'side';
+      sideDecks[j].push(newCard);
+    }
+  }
+};
 
+function dealHands(){
+  
+};
 
+var hands = [[],[]];
+var sideDecks = [[],[]];
 var mainDeck = [];

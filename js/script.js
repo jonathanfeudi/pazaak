f69514fcb99f392c9f1cbec156cbe2d0087651aa
Jonathan Feudi <jonathanfeudi@gmail.com>
2016-02-03 14:27:30 -0500

    added clearRound function to game object. Clears hands, decks, everything in play, and all associated values to prepare for next round.

--- js/script.js
+++ js/script.js
@@ -28,6 +28,12 @@ function makeMainDeck(){
   }
 };
 
+function clearMainDeck(){
+  for (var i = 0; i < mainDeck.length; i++){
+    mainDeck.splice(0, mainDeck.length);
+  }
+};
+
 function makeSideDeck(){
   for (var j = 0; j < 2; j++){
     for (var i = 0; i < 10; i++){
@@ -47,6 +53,15 @@ function makeSideDeck(){
   }
 };
 
+function clearSideDecks(){
+  for (var i = 0; i < sideDecks[0].length; i++){
+    sideDecks[0].splice(0, sideDecks[0].length);
+  }
+  for (var i = 0; i < sideDecks[1].length; i++){
+    sideDecks[1].splice(0, sideDecks[1].length);
+  }
+};
+
 function dealHands(){
   for (var j = 0; j < 2; j++){
     for (var i = 0; i < 4; i++){
@@ -57,6 +72,15 @@ function dealHands(){
     }
   }
 };
+
+function clearHands(){
+  for (var i = 0; i < hands[0].length; i++){
+    hands[0].splice(0, hands[0].length);
+  }
+  for (var j = 0; j < hands[1].length; j++){
+    hands[1].splice(0, hands[1].length);
+  }
+}
 
 var player = {
   standing: [[false],[false]],
@@ -74,7 +98,7 @@ var player = {
   },
   startTurn: function(){
     var x = game.whosTurn;
-    if ((player.standing[0][0] === true) && (player.standing[1][0])){
+    if ((player.standing[0][0]) && (player.standing[1][0])){
       game.checkWin();
       return;
     }
@@ -109,7 +133,7 @@ var player = {
     player.scoreArray[x].push(total);
   },
   cardArray: [[],[]],
-  scoreArray: [[],[]],
+  scoreArray: [[0],[0]],
 };
 
 var game = {
@@ -135,10 +159,31 @@ var game = {
     if (((player.scoreArray[0][0] > player.scoreArray[1][0]) && !(player.scoreArray[0][0] > 20)) || (!(player.scoreArray[0][0] > 20) && (player.scoreArray[1][0] > 20))){
       console.log('Player 1 Wins');
       game.wins[0][0] += 1;
+      game.clearRound();
     } else if (((player.scoreArray[1][0] > player.scoreArray[0][0]) && !(player.scoreArray[1][0] > 20)) || (!(player.scoreArray[1][0] > 20) && (player.scoreArray[0][0] > 20))){
       console.log('Player 2 Wins');
       game.wins[1][0] += 1;
+      game.clearRound();
     }
+  },
+  clearRound: function(){
+    player.scoreArray[0][0] = 0;
+    player.scoreArray[1][0] = 0;
+    for (var i = 0; i < player.cardArray[0].length; i++){
+      player.cardArray[0].splice(0, player.cardArray[0].length);
+    }
+    for (var j = 0; j < player.cardArray[1].length; j++){
+      player.cardArray[1].splice(0, player.cardArray[1].length);
+    }
+    for (var h = 0; h < inPlay[0].length; h++){
+      inPlay[0].splice(0, inPlay[0].length);
+    }
+    for (var k = 0; k < inPlay[1].length; k++){
+      inPlay[1].splice(0, inPlay[1].length);
+    }
+    clearMainDeck();
+    clearSideDecks();
+    clearHands();
   },
 };
 
